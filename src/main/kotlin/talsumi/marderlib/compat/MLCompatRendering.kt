3d0dab61e526546e191482084bf7833b2cdf158b
Talsumi <92010474+Talsumi@users.noreply.github.com>
2022-11-13 14:55:47 +1100

    1.18 & 1.19 cross compat

---
+++ src/main/kotlin/talsumi/marderlib/compat/MLCompatRendering.kt
@@ -0,0 +1,28 @@
+package talsumi.marderlib.compat
+
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.render.BufferBuilder
+import net.minecraft.client.render.BufferRenderer
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory
+import net.minecraft.client.render.entity.EntityRendererFactory
+import net.minecraft.client.render.entity.EntityRendererFactory.Context
+
+object MLCompatRendering {
+
+    fun drawBuffer(builder: BufferBuilder)
+    {
+        BufferRenderer.drawWithShader(builder.end())
+    }
+
+    fun makeEntityRendererContext(): EntityRendererFactory.Context
+    {
+        val mc = MinecraftClient.getInstance()
+        return EntityRendererFactory.Context(mc.entityRenderDispatcher, mc.itemRenderer, mc.blockRenderManager, mc.entityRenderDispatcher.heldItemRenderer, mc.resourceManager, mc.entityModelLoader, mc.textRenderer)
+    }
+
+    fun makeBlockEntityRendererContext(): BlockEntityRendererFactory.Context
+    {
+        val mc = MinecraftClient.getInstance()
+        return BlockEntityRendererFactory.Context(mc.blockEntityRenderDispatcher, mc.blockRenderManager, mc.itemRenderer, mc.entityRenderDispatcher, mc.entityModelLoader, mc.textRenderer)
+    }
+}
---
+++ src/main/kotlin/talsumi/marderlib/compat/MLCompatText.kt
@@ -0,0 +1,15 @@
+package talsumi.marderlib.compat
+
+import net.minecraft.text.MutableText
+import net.minecraft.text.Text
+
+object MLCompatText {
+
+    fun makeText(contents: String): Text = Text.of(contents)
+
+    fun makeLiteralText(contents: String): MutableText = Text.literal(contents)
+
+    fun makeTranslatableText(contents: String): MutableText = Text.translatable(contents)
+
+    fun makeTranslatableText(contents: String, args: Array<Any>): MutableText = Text.translatable(contents, args)
+}
